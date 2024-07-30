7f6d000ea6a9c9fe35e1ac548b5f4cf0cc93dffc
Kishan Waghela <Kishan.Waghela@teknopoint.local>
2024-07-30 15:16:55 +0530

    WIP

--- blocks/glider/glider.js
+++ blocks/glider/glider.js
@@ -2,31 +2,46 @@ import Glider from "../../common/carousel/glider.min.js";
 
 export default function decorate(block) {
   const carouselitem = Array.from(block.children);
+  let config = {},
+    prev,
+    next;
   const gliderContainer = document.createElement("div");
+  gliderContainer.dataset.gliderContainer = "glider-container";
   const dots = document.createElement("div");
-  for (let index = 0; index < block.children.length; index++) {
-    const element = block.children[index];
-    gliderContainer.append(element);
-  }
-  //   carouselitem.forEach(function (item) {
-  //   });
+  carouselitem.forEach(function (item, index) {
+    if (carouselitem.length === index + 1) {
+      let [configData, prevEl, nextEl] = item.children;
+      config = JSON.parse(configData.innerText);
+      prev = prevEl;
+      next = nextEl;
+      prev.classList.add("glider-prev");
+      next.classList.add("glider-next");
+    } else {
+      item.dataset.rowIndex = "row-" + index;
+      const childwrap = document.createElement("div");
+      childwrap.dataset.cloumnWrapper = "cloumn-wrapper";
+      Array.from(item.children).forEach(function (el, index) {
+        el.dataset.cloumnIndex = "cloumn-" + index;
+        childwrap.append(el);
+      });
+      item.append(childwrap);
+      gliderContainer.append(item);
+    }
+  });
   block.innerHTML = "";
   block.appendChild(gliderContainer);
   block.append(dots);
+  block.append(prev);
+  block.append(next);
 
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        new Glider(block.children[0], {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          draggable: true,
-          dots,
-          // arrows: {
-          //   prev: ".glider-prev",
-          //   next: ".glider-next",
-          // },
-        });
+        config.dots = config.dots && dots;
+        config.arrows = {};
+        config.arrows.prev = config.prev && prev;
+        config.arrows.next = config.next && next;
+        new Glider(block.children[0], config);
       }
     });
   });
