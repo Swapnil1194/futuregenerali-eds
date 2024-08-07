16b73f706ab5ad116ea6c87861d60a19fcabbbee
Devmani Mishra <Devmani.Mishra@teknopoint.local>
2024-08-07 12:37:26 +0530

    glider and swiper code

--- blocks/swiper/swiper.js
+++ blocks/swiper/swiper.js
@@ -1,14 +1,14 @@
 
-import Swiper from '../../common/swiper/swiper.min.js';
+import swiperMin from '../../common/swiper/swiper.min.js';
 import { embedYoutube, loadEmbed } from '../embed/embed.js';
-
+swiperMin();
 export default function decorate(block) {
   const carouselitem = Array.from(block.children);
   let config = {};
   let prev;
   let next;
   const swiperContainer = document.createElement('div');
-  swiperContainer.dataset.swiperContainer = 'swiper-container';
+  swiperContainer.dataset.swiperContainer = 'swiper-wrapper';
   const dots = document.createElement('div');
   carouselitem.forEach((item, index) => {
     if (carouselitem.length === index + 1) {
@@ -20,6 +20,7 @@ export default function decorate(block) {
       next.classList.add('swiper-next');
     } else {
       item.dataset.rowIndex = `row-${index}`;
+      item.classList.add('swiper-slide');
       const childwrap = document.createElement('div');
       childwrap.dataset.columnWrapper = 'column-wrapper';
       Array.from(item.children).forEach((el, index) => {
@@ -33,7 +34,8 @@ export default function decorate(block) {
   //         childwrap.append(div);
           childwrap.append(loadEmbed(el, link));
         }else{
-          childwrap.append(el);
+            block.classList.add('mySwiper');
+            childwrap.append(el);
         }
       });
       item.append(childwrap);
@@ -41,19 +43,40 @@ export default function decorate(block) {
     }
   });
   block.innerHTML = '';
+  block.closest('.swiper-wrapper').classList.add('swiperContainer');
+  block.closest('.swiper-wrapper').classList.remove('swiper-wrapper');
   block.appendChild(swiperContainer);
-  block.append(dots);
-  block.append(prev);
-  block.append(next);
+//   block.append(dots);
+//   block.append(prev);
+//   block.append(next);
 
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        config.dots = config.dots && dots;
-        config.arrows = {};
-        config.arrows.prev = config.prev && prev;
-        config.arrows.next = config.next && next;
-        new Swiper(block, config);
+        // config.dots = config.dots && dots;
+        // config.arrows = {};
+        // config.arrows.prev = config.prev && prev;
+        // config.arrows.next = config.next && next;
+        new Swiper(".mySwiper", {
+            effect: "coverflow",
+            grabCursor: true,
+            centeredSlides: true,
+            loop:true,
+            slidesPerView: "auto",
+            coverflowEffect: {
+              rotate: 0,
+              stretch: 0,
+              depth: 150,
+              modifier: 2.5,
+              slideShadows: true,
+            },
+            autoplay:{
+          
+              delay:44000,
+              disableOnInteraction:false,
+            }
+          
+          });
       }
     });
   });
